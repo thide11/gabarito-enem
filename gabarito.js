72f401bf9a43f07bbc36b3a35e899f809f23e6db
Thiago Viana <thide2001@gmail.com>
2020-04-05 02:44:37 -0300

    Commit inicial

---
+++ gabarito.js
@@ -0,0 +1,91 @@
+
+//gabarito das linguas estrangeiras
+const gabaritoLinguasEstrangeiras = [
+[
+["Gabarito Amarelo Lingua Inglesa - Questões corrigidas pelo Sistema COC de Ensino (G1)", "B", "B", "E", "D", "C"],
+["Gabarito Amarelo Lingua Espanhola - Questões corrigidas pelo Sistema COC de Ensino (G1)", "C", "B", "B", "D", "B"]
+],
+[
+["Gabarito Azul Lingua Inglesa - Questões corrigidas pelo Sistema COC de Ensino (G1)", "B", "B", "D", "E", "C"],
+["Gabarito Azul Lingua Espanhola - Questões corrigidas pelo Sistema COC de Ensino (G1)", "B", "B", "C", "D", "B"]
+], 
+[
+["Gabarito Rosa Lingua Inglesa - Questões corrigidas pela equipe Chromos", "C", "B", "E", "D", "B"],
+["Gabarito Rosa Lingua Espanhola - Questões corrigidas pela equipe Chromos", "B", "B", "D", "C", "B"]
+],
+[
+["Gabarito Branco Lingua Inglesa - Questões corrigidas pelo Sistema COC de Ensino (G1)", "B", "C", "D", "E", "B"],
+["Gabarito Branco Lingua Espanhola - Questões corrigidas pelo Sistema COC de Ensino (G1)", "D", "B", "C", "B", "B"]
+]
+];
+
+console.log('ta conectado!');
+console.log(gabaritoLinguasEstrangeiras);
+
+
+//o gabarito comeca a partida da questao 6
+const gabaritoTodasCores = [
+//gabarito amarelo
+//questoes 6 a 15
+["B", "B", "C", "A", "D", "B", "C", "A", "C", "E",
+//questoes 16 a 25
+ "D", "B", "E", "C", "D", "A", "B", "A", "A", "B",
+//questoes 26 a 35
+  "C", "B", "A", "E", "C", "C", "D", "E", "B", "B",
+//questoes 36 a 45
+  "D", "D", "E", "A", "D", "D", "C", "E", "C", "B", 
+//questtoes 46 a 55
+  "A", "E", "E", "C", "D", "E", "C", "B", "E", "B",
+//questoes 56 a 65
+  "D", "C", "A", "C", "d", "E", "D", "E", "E", "D",
+//questoes 66 a 75
+  "B", "D", "d", "E", "B", "C", "E", "C", "D", "E",
+//questoes 76 a 85
+  "C", "A", "A", "B", "E", "D", "B", "E","B", "B",
+//questoes 86 a 90
+  "E", "B", "E", "C", "C"],
+
+//gabarito Azul
+//questoes 6 a 15
+["C", "E", "C", "B", "B", "B", "D", "B", "A", "A",
+//questoes 16 a 25
+	"A", "D", "D", "D", "E", "B", "B", "C", "D", "D",
+//questoes 26 a 35
+	"B", "C", "A", "C", "E", "D", "B", "E", "B", "C",
+//questoes 36 a 45
+	"B", "C", "D", "E", "A", "E", "C", "C", "D", "A",
+//questtoes 46 a 55
+	"C", "A", "C", "D", "D", "B", "D", "D", "E", "C",
+//questoes 56 a 65
+	"C", "E", "D", "E", "E", "E", "B", "C", "E", "A",
+//questoes 66 a 75
+	"E", "E", "C", "B", "B", "E", "B", "E", "D", "B",
+//questoes 76 a 85
+	"E", "D", "E", "C", "B", "E", "B", "D", "C", "A",
+//questoes 86 a 90
+	"A", "B", "C", "D", "E"],
+//gabarito Rosa 
+["E", "C", "A", "D", "B", "B", "B", "E", "C", "B", "E", "A", "E", "C", "A", "B", "D", "A", "D", "A", "C", "D", "B", "A", "B", "C", "E", "C", "B", "B", "E", "C", "A", "D", "A", "B", "E", "C", "E", "D",
+//ciencias humanas agora
+"C", "D", "E", "C", "A", "A", "B", "B", "D", "D", "E", "C", "B", "E", "E", "D", "B", "E", "B", "B", "E", "B", "E", "D", "E", "E", "E", "B", "C", "E", "A", "E", "E", "C", "E", "C", "C", "D", "B", "D", "D", "C", "A", "C", "D"
+],
+//gabarito Branco 
+//questoes 6 a 15
+["A", "D", "D", "C", "D", "E", "A", "E", "C", "B",
+//questoes 16 a 25
+	"C", "B", "A", "D", "B", "B", "C", "C", "E", "C",
+//questoes 26 a 35
+	"B", "D", "E", "B", "E", "B", "B", "D", "C", "D",
+//questoes 36 a 45
+	"A", "C", "E", "D", "B", "C", "A", "B", "A", "A",
+//questtoes 46 a 55
+	"E", "C", "C", "B", "B", "E", "B", "E", "D", "B",
+//questoes 56 a 65
+	"E", "D", "B", "D", "D", "A", "E", "E", "C", "D",
+//questoes 66 a 75
+	"E", "C", "B", "E", "C", "A", "C", "D", "E", "D",
+//questoes 76 a 85
+	"E", "E", "C", "D", "E", "C", "A", "A", "B", "B",
+//questoes 86 a 90
+	"D", "E", "B", "C", "E"],
+];
